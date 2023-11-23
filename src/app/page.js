5f15dc088203bb1075b0d27109dd16e1b6826f54
root <root@DESKTOP-7KIGB8O>
2023-11-23 14:54:31 -0500

    agregar seccion de acerca y de servicios

--- src/app/page.js
+++ src/app/page.js
@@ -28,6 +28,59 @@ export default function Home() {
                 </div>
                 <button className={styles.actionButton}>APRENDER</button>
             </section>
+            <section className={styles.aboutSection}>
+                <div className={styles.aboutImageContainer}>
+                    <Image
+                        src="/images/home/logo150c.png"
+                        alt="About Image"
+                        width={500}
+                        height={500}
+                        className={styles.aboutImage}
+                    />
+                </div>
+                <p className={styles.aboutTitle}>- ACERCA DE AS RESEARCH -</p>
+                <p className={styles.aboutText}>
+                    Breve presentación de la organización, útil para introducir
+                    en pocas líneas los servicios o lo que los diferencia.
+                </p>
+                <p className={styles.aboutSubtext}>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                    sed diam nonummy.
+                </p>
+            </section>
+            <section className={styles.servicesSection}>
+                <p className={styles.servicesTitle}>- SERVICIOS -</p>
+                <p className={styles.servicesText}>¿Con qué trabajamos?</p>
+                <div className={styles.cardContainer}>
+                    <div className={styles.card}>
+                        <p className={styles.title}>Machine Learning</p>
+                        <p className={styles.text}>
+                            This is a longer card with supporting text below as
+                            a natural lead-in to additional content. This
+                            content is a little bit longer.
+                        </p>
+                        <button className={styles.button}>LEER MÁS</button>
+                    </div>
+                    <div className={styles.card}>
+                        <p className={styles.title}>Big Data</p>
+                        <p className={styles.text}>
+                            This is a longer card with supporting text below as
+                            a natural lead-in to additional content. This
+                            content is a little bit longer.
+                        </p>
+                        <button>LEER MÁS</button>
+                    </div>
+                    <div className={styles.card}>
+                        <p className={styles.title}>Minería de Datos</p>
+                        <p className={styles.text}>
+                            This is a longer card with supporting text below as
+                            a natural lead-in to additional content. This
+                            content is a little bit longer.
+                        </p>
+                        <button className={styles.button}>LEER MÁS</button>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
